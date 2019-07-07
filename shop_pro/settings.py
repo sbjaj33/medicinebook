54df6a95572eece36272d375a9593eff8e3f78a9
sbjaj33 <sbjaj33@gmail.com>
2019-07-07 23:47:50 +0530

    Updated Settings

--- shop_pro/settings.py
+++ shop_pro/settings.py
@@ -19,11 +19,10 @@ STATIC_DIR=os.path.join(BASE_DIR,'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
-export SECRET_KEY='2623f53c3472c6991b59b20e1cabc7a5'
-export DEBUG_VALUE="True"
+
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'av(fri&(d5-n_twh2)5&&el-6qi9*ekgjr1je2udv+qnxs8faq'
-SECRET_KEY=os.environ.get('SECRET_KEY')
+SECRET_KEY = 'av(fri&(d5-n_twh2)5&&el-6qi9*ekgjr1je2udv+qnxs8faq'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
